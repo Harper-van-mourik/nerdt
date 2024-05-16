5158f6437b69f358670814d701ff928616f6c808
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-16 21:50:12 +0200

    added a start of the desktop menu

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    <nav class="flex justify-center py-4">
-      <img src="~/assets/img/nerdt-logo.svg" alt="nerdt logo" class="w-20" />
-    </nav>
-  </div>
+  <nav class="md:container">
+    <div class="flex items-center justify-between py-4">
+      <a
+        href="/"
+        class="rounded-md focus:outline-none focus:ring focus:ring-violet-300"
+      >
+        <img src="~/assets/img/nerdt-logo.svg" alt="nerdt logo" class="w-20" />
+      </a>
+
+      <div class="flex items-center gap-4">
+        <div class="flex items-center gap-2">
+          <BaseLink size="sm">snippets</BaseLink>
+          <BaseLink size="sm">about</BaseLink>
+        </div>
+        <BaseButton size="sm">Cool and now?</BaseButton>
+      </div>
+    </div>
+  </nav>
 </template>
