084b8c2ef3323a7eefc9bc274c7905447d99adfd
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-16 21:50:21 +0200

    An app Hero

---
+++ components/AppHero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="container flex flex-col items-center justify-center h-64 lg:container"
+  >
+    <div class="flex flex-col items-center max-w-md gap-2 text-center">
+      <h1 class="text-xl font-bold">
+        nerdt.nl - Your guide in front-end development
+      </h1>
+      <h1 class="">
+        On nerdt you'll find the best Tailwind styling tricks, complete copy &
+        paste code, not even a keyboard needed.
+      </h1>
+
+      <BaseButton>Shut up, give me the goodies.</BaseButton>
+    </div>
+  </div>
+</template>
