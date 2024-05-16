75a1a03cff5046ebd4a5710907d786568faa4dbc
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-16 21:49:44 +0200

    Base button with basic size

---
+++ components/Base/BaseButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface ButtonProps {
+  size: "sm" | "md";
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  size: "md",
+});
+
+const buttonClasses = computed(() => {
+  switch (props.size) {
+    case "sm":
+      return "px-2 py-1 rounded";
+    default:
+      return "px-3 py-2 rounded-md";
+  }
+});
+</script>
+
+<template>
+  <button
+    class="text-sm text-white shadow bg-slate-950 hover:bg-slate-800 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-300"
+    :class="buttonClasses"
+  >
+    <slot />
+  </button>
+</template>
