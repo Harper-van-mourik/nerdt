7c3e213c81475c7bea0efd7fe7fbd45e917f800a
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-16 21:49:54 +0200

    Base link component

---
+++ components/Base/BaseLink.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="px-2 py-1 text-sm rounded hover:underline active:text-slate-700 focus:outline-none focus:ring focus:ring-slate-300"
+  >
+    <slot />
+  </button>
+</template>
