11e09fcaa941872c55f824b0e1aa7fe5c98c21ea
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-17 16:34:01 +0200

    nav outline change

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,19 +1,19 @@
 <template>
-  <nav class="md:container">
+  <nav class="container md:container">
     <div class="flex items-center justify-between py-4">
       <a
         href="/"
-        class="rounded-md focus:outline-none focus:ring focus:ring-violet-300"
+        class="rounded-md focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-offset-4"
       >
         <img src="~/assets/img/nerdt-logo.svg" alt="nerdt logo" class="w-20" />
       </a>
 
       <div class="flex items-center gap-4">
         <div class="flex items-center gap-2">
-          <BaseLink size="sm">snippets</BaseLink>
-          <BaseLink size="sm">about</BaseLink>
+          <BaseNavLink href="/posts">Posts</BaseNavLink>
+          <BaseNavLink href="/about">About</BaseNavLink>
         </div>
-        <BaseButton size="sm">Cool and now?</BaseButton>
+        <BaseButton size="sm">Login</BaseButton>
       </div>
     </div>
   </nav>
