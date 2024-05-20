b2011e379393449ee99737f02817c25a1f614e08
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-20 16:14:36 +0200

    news card animations

--- composables/usePosts.ts
+++ composables/usePosts.ts
@@ -3,6 +3,7 @@ export interface Posts {
   slug: string;
   title: string;
   content: string;
+  featured_image: string;
   time: number;
 }
 
@@ -11,14 +12,20 @@ export const posts: Posts[] = [
     id: 0,
     slug: "first-post",
     title: "First post",
-    content: "Hello first post",
+    content:
+      "Hello first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent suscipit tortor in diam ornare, eu aliquet lacus maximus. Pellentesque enim ante, euismod quis ante in, ullamcorper fringilla diam.",
+    featured_image:
+      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     time: 1715947218257,
   },
   {
     id: 1,
     slug: "second-post",
     title: "Second posted post",
-    content: "The second time",
+    content:
+      "CSS animations like no other. Donec egestas tellus quis viverra semper. Duis at lectus leo. Aenean tristique porta enim, eget aliquam nisi semper sit amet. ",
+    featured_image:
+      "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?q=80&w=4170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     time: 1715947218257,
   },
 ];
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -3,21 +3,29 @@ const posts = usePosts();
 </script>
 
 <template>
-  <div class="container md:container">
-    <h1 class="text-lg font-bold">Posts</h1>
-    <div class="grid grid-cols-4 gap-6">
-      <NuxtLink
-        :to="`/posts/${slug}`"
-        v-for="{ title, content, slug } in posts"
-        class="rounded-md focus-visible:outline-2 focus-visible:outline-dashed focus-visible:outline-offset-2"
-      >
-        <BaseCard>
-          <div class="p-6">
+  <section class="py-24 text-white bg-black">
+    <div class="container flex flex-col gap-8 md:container">
+      <div>
+        <h1 class="text-lg font-bold">Posts</h1>
+        <p>All news</p>
+      </div>
+      <div class="grid grid-cols-4 gap-6">
+        <NuxtLink
+          :to="`/posts/${slug}`"
+          v-for="{ title, content, slug, featured_image } in posts"
+          :style="`background-image: url('${featured_image}')`"
+          class="flex flex-col justify-end h-48 p-2 overflow-hidden transition-all ease-in-out bg-center bg-no-repeat bg-cover rounded-xl focus-visible:outline-2 focus-visible:outline-dashed focus-visible:outline-offset-2 group hover:p-0"
+        >
+          <div
+            class="flex flex-col w-full p-6 text-white transition-all ease-in-out group-hover:gap-2 group-hover:p-8 rounded-xl group-hover:rounded-none bg-gradient-to-r from-black/70 to-black/30 backdrop-blur-sm group-hover:h-full"
+          >
             <div class="font-bold">{{ title }}</div>
-            <div>{{ content }}</div>
+            <div class="line-clamp-1 group-hover:line-clamp-4">
+              {{ content }}
+            </div>
           </div>
-        </BaseCard>
-      </NuxtLink>
+        </NuxtLink>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
