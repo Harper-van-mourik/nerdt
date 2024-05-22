9f210aa787d6961dfeaa5b583351fc637f4264a2
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-22 13:57:39 +0200

    Tiptap Editor

--- pages/gate/post/new.vue
+++ pages/gate/post/new.vue
@@ -1,38 +1,30 @@
 <template>
   <div class="container flex justify-center md:container">
-    <BaseCard>
-      <div class="flex flex-col gap-6 p-4 max-w-64 border-slate-200">
-        <div class="text-xl font-bold text-center">New post</div>
-        <hr />
+    <div
+      class="flex flex-col w-full max-w-screen-md gap-6 p-4 border-slate-200"
+    >
+      <div class="text-xl font-bold text-center">New post</div>
 
-        <form class="flex flex-col gap-4">
-          <label class="block">
-            <span class="block mb-2 text-sm font-medium text-slate-600"
-              >Title</span
-            >
-
-            <input
-              type="text"
-              class="block w-full px-3 py-2 text-sm bg-white border rounded-md shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-            />
-          </label>
-
-          <label class="block">
-            <span class="block mb-2 text-sm font-medium text-slate-60"
-              >Content</span
-            >
-
-            <textarea
-              name="content"
-              rows="4"
-              cols="50"
-              class="block w-full px-3 py-2 text-sm bg-white border rounded-md shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-            />
-          </label>
-
-          <BaseButton submit>send</BaseButton>
-        </form>
+      <div class="flex justify-end gap-4">
+        <BaseButton submit>Create post</BaseButton>
       </div>
-    </BaseCard>
+      <form class="flex flex-col gap-4" @submit.prevent="">
+        <label class="block">
+          <span class="block mb-2 text-sm font-medium text-slate-600"
+            >Title</span
+          >
+
+          <input
+            type="text"
+            placeholder="Enter your title"
+            class="block w-full px-3 py-2 text-sm bg-white border rounded-md shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+          />
+        </label>
+
+        <div>
+          <TiptapEditor />
+        </div>
+      </form>
+    </div>
   </div>
 </template>
