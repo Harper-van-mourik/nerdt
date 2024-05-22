010de8beded084e8587de33a6e81fcb7ca9294e5
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-22 23:39:13 +0200

    Added firebase + route gaurd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-vuefire"],
+  vuefire: {
+    auth: {
+      enabled: true,
+    },
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    },
+  },
+});
