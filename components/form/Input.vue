044c758f7811492178b74ed90a5086c0965e7198
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-23 16:12:09 +0200

    No placeholder by default,  FormKit is on the horizon

--- components/form/Input.vue
+++ components/form/Input.vue
@@ -15,7 +15,6 @@ const model = defineModel();
     <input
       v-model="model"
       type="text"
-      placeholder="Enter your title"
       v-bind="attrs"
       class="block w-full px-3 py-2 text-sm bg-white border rounded-md shadow-sm focus:outline-2 border-slate-300 focus:outline-dashed placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none focus:outline-offset-2 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
       required
