e65664024165bac765024e7684be9ea354954a0e
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-23 20:01:37 +0200

    overview of focus classes

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+const classes = [
+  "[&_a:focus-visible]:outline-2 [&_a:focus-visible]:outline-offset-2 [&_a:focus-visible]:outline-dashed",
+  "[&_button:focus-visible]:outline-2 [&_button:focus-visible]:outline-offset-2 [&_button:focus-visible]:outline-dashed",
+];
+</script>
+
 <template>
-  <div
-    class="h-full text-slate-950 [&_a:focus-visible]:outline-2 [&_a:focus-visible]:outline-offset-2 [&_a:focus-visible]:outline-dashed"
-  >
+  <div class="h-full text-slate-950" :class="classes">
     <NuxtPage />
   </div>
 </template>
