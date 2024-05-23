5a536d1b8ba38432000cfccf20b51aeb000ad01f
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-23 20:02:08 +0200

    If it's not a a link, it's a button but looks like a link.

--- components/Base/BaseLink.vue
+++ components/Base/BaseLink.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+interface Link {
+  to?: string;
+}
+
+const props = withDefaults(defineProps<Link>(), {});
+
+const componentToShow = computed(() => {
+  if (props.to) {
+    return resolveComponent("NuxtLink");
+  }
+  return "button";
+});
+</script>
+
 <template>
-  <NuxtLink class="rounded hover:underline active:text-slate-700">
-    <slot />
-  </NuxtLink>
+  <component
+    :is="componentToShow"
+    :to="to"
+    class="rounded hover:underline active:text-slate-700"
+  >
+    <slot></slot>
+  </component>
 </template>
