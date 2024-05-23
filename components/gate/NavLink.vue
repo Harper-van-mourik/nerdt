8ca3536d21b6a43397d6b6c79c479522bd0a4f82
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-23 16:16:34 +0200

    GateNavLink dynamic link or button

--- components/gate/NavLink.vue
+++ components/gate/NavLink.vue
@@ -1,7 +1,24 @@
+<script setup lang="ts">
+interface NavLink {
+  to?: string;
+}
+
+const props = withDefaults(defineProps<NavLink>(), {});
+
+const componentToShow = computed(() => {
+  if (props?.to) {
+    return resolveComponent("NuxtLink");
+  }
+  return "button";
+});
+</script>
+
 <template>
-  <NuxtLink
+  <component
+    :is="componentToShow"
+    :to="to"
     class="flex items-center justify-center w-full px-2 rounded-2xl aspect-square hover:bg-slate-900"
   >
     <slot></slot>
-  </NuxtLink>
+  </component>
 </template>
