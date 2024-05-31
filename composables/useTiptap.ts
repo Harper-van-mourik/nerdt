baea341ed17ed7e8f83a4b72069c0b04512c5b7c
Harper van Mourik <mourik.v.h@gmail.com>
2024-05-31 19:49:03 +0200

    text-size in code

--- composables/useTiptap.ts
+++ composables/useTiptap.ts
@@ -2,7 +2,7 @@
 // Dyanmically created Tailwind classes will get purged by Tailwind's JIT.
 export const tiptapClasses: string[] = [
   "[&_pre]:p-2 [&_pre]:rounded [&_pre]:bg-black [&_pre]:text-white",
-  "[&_.tiptap>:not(pre)_code]:bg-slate-200 [&_.tiptap>:not(pre)_code]:rounded [&_.tiptap>:not(pre)_code]:px-2 [&_.tiptap>:not(pre)_code]:py-1",
+  "[&_.tiptap>:not(pre)_code]:bg-slate-200 [&_.tiptap>:not(pre)_code]:rounded [&_.tiptap>:not(pre)_code]:px-2 [&_.tiptap>:not(pre)_code]:py-1 [&_.tiptap>:not(pre)_code]:text-sm",
   "[&_code]:font-xs",
   "[&_img]:rounded-xl",
   "[&_a]:underline",
