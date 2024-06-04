7a934cb24da7bedbe89e058db53fa82242ae606e
Harper van Mourik <mourik.v.h@gmail.com>
2024-06-04 15:57:53 +0200

    current user on gate admin page

--- pages/gate/index.vue
+++ pages/gate/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { useCurrentUser } from "vuefire";
+const user = useCurrentUser();
+</script>
+
 <template>
   <div class="text-xl font-bold">Admin's gate</div>
+  <p>
+    Logged in as: <span class="font-bold">{{ user?.email }}</span>
+  </p>
+
+  <p>
+    id: <span class="font-bold">{{ user?.uid }}</span>
+  </p>
 </template>
